d909f8861ef142a17bce6757e7fa0034e7a67e9b
fns92 <sanchez36822171@gmail.com>
2018-05-02 15:21:28 -0300

    FS: El metodo que detecta el saludo

--- JohnVonNeumann/src/main/java/bot/Asistente.java
+++ JohnVonNeumann/src/main/java/bot/Asistente.java
@@ -16,6 +16,21 @@ public class Asistente {
 		}
 		return "";
 	}
+	
+	
+	public String devolverSaludo(String entrada) {
+		
+		String[] palabras = {"hola", "buen dia", "buenas", "hey"};
+		for(int i = 0; i < palabras.length; i++){
+			
+			if(entrada.toLowerCase().contains(palabras[i])) {
+				
+				return "Hola, UserX!";   /*Luego puede devolver una respuesta random*/
+			}
+		}
+		return "";
+		
+	}
 
 
 }
